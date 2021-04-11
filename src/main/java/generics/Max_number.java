655e7489942dee2ae4651b1457b1614cb572c5d7
MrunalNagalwade <mrunalnagalwade187@gmail.com>
2021-04-11 10:57:34 -0700

    [Mrunal:]Find maximum float value

--- src/main/java/generics/Max_number.java
+++ src/main/java/generics/Max_number.java
@@ -16,4 +16,19 @@ public class Max_number {
             return secondnumber;
         }
     }
+    public <E extends Float> Float checkmaxnumFloat(E firstnumber, E secondnumber, E thirdnumber)
+    {
+        if (firstnumber.compareTo(secondnumber) > 0 )
+        {
+            return firstnumber;
+        }
+        else if (thirdnumber.compareTo(secondnumber) > 0)
+        {
+            return thirdnumber;
+        }
+        else
+        {
+            return secondnumber;
+        }
+    }
 }
--- src/test/java/generics/Max_Number_Test.java
+++ src/test/java/generics/Max_Number_Test.java
@@ -5,12 +5,21 @@ import org.junit.jupiter.api.Test;
 
 public class Max_Number_Test {
     @Test
-    public void givenThreeInput_WhenProperfirst_Number_ReturnTrue() {
+    public void givenThreeInput_WhenProper_first_Number_ReturnTrue() {
         Integer firstnumber = 9;
         Integer secondnumber = 1;
         Integer thirdnumber = 4;
         Max_number max_number = new  Max_number();
         Integer maxNum = max_number.checkmaxnum(firstnumber,secondnumber,thirdnumber);
         Assertions.assertEquals((Integer)9,maxNum);
+    }
+    @Test
+    public void givenThreeInput_WhenProper_second_Number_ReturnTrue() {
+        Float firstnumber = 67.7f;
+        Float secondnumber = 90.3f;
+        Float thirdnumber = 12.4f;
+        Max_number max_number = new  Max_number();
+        Float maxNum = max_number.checkmaxnumFloat(firstnumber,secondnumber,thirdnumber);
+        Assertions.assertEquals((Float)90.3f,maxNum);
     }
 }
